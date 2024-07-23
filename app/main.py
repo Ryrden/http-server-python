532af9e2e38f456b510b44924d3985a530ad5329
Ryan S <ryanssteles@gmail.com>
2024-07-22 22:21:19 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
--- your_server.sh
+++ your_server.sh
@@ -5,4 +5,4 @@
 # CodeCrafters uses this file to test your code. Don't make any changes here!
 #
 # DON'T EDIT THIS!
-exec pipenv run python3 -m app.main "$@"
+pipenv run python3 -m app.main "$@"
