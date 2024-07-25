9cddd25e161139ee14b24e3c5ffe3c5a92ecef87
Ryan S <ryanssteles@gmail.com>
2024-07-25 12:00:54 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import re
 import socket
 
 
@@ -9,8 +10,11 @@ def main():
     request, *_ = data.split("\r\n") 
     [_, path, _] = request.split(" ")
 
+    echo_regex = re.compile(r"^/echo/(.*)$")
     if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
+    elif match := echo_regex.match(path):
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(match.group(1))}\r\n\r\n{match.group(1)}"
     elif path != "/":
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
