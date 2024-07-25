5c2534bbc29673032d47d80e35bb4f8a7dab3431
Ryan S <ryanssteles@gmail.com>
2024-07-25 12:57:37 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,14 +6,17 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, _ = server_socket.accept()
 
-    data = client.recv(1024).decode()
-    request, *_ = data.split("\r\n") 
-    [_, path, _] = request.split(" ")
-
-    echo_regex = re.compile(r"^/echo/(.*)$")
+    data = client.recv(1024).decode().strip()
+    request = data.split("\r\n")
+    path = request[0].split(" ")[1]
+    
     if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
-    elif match := echo_regex.match(path):
+    elif path == "/user-agent":
+        user_agent = request[2].split(":")[1].strip()
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+    elif path.startswith("/echo/"):
+        match = re.compile(r"^/echo/(.*)$").match(path)
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(match.group(1))}\r\n\r\n{match.group(1)}"
     elif path != "/":
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
