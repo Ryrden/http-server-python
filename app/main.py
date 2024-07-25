fbd9ff7b03db3fe07ffe312751f8911c10e7efba
Ryan S <ryanssteles@gmail.com>
2024-07-25 14:06:04 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,29 +1,42 @@
 import re
 import socket
+import threading
 
-
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client, _ = server_socket.accept()
-
-    data = client.recv(1024).decode().strip()
+def handle_request(data):
     request = data.split("\r\n")
     path = request[0].split(" ")[1]
     
     if path == "/":
-        response = "HTTP/1.1 200 OK\r\n\r\n"
-    elif path == "/user-agent":
+        return "HTTP/1.1 200 OK\r\n\r\n"
+    
+    if path == "/user-agent":
         user_agent = request[2].split(":")[1].strip()
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-    elif path.startswith("/echo/"):
-        match = re.compile(r"^/echo/(.*)$").match(path)
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(match.group(1))}\r\n\r\n{match.group(1)}"
-    elif path != "/":
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+    
+    if path.startswith("/echo/"):
+        match = re.match(r"^/echo/(.*)$", path)
+        if match:
+            echo_message = match.group(1)
+            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_message)}\r\n\r\n{echo_message}"
+    
+    return "HTTP/1.1 404 Not Found\r\n\r\n"
 
-    client.send(response.encode())
-    client.close()
+def client_handler(client):
+    try:
+        data = client.recv(1024).decode().strip()
+        if data:
+            response = handle_request(data)
+            client.send(response.encode())
+    finally:
+        client.close()
 
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket.listen()
+    
+    while True:
+        client, _ = server_socket.accept()
+        threading.Thread(target=client_handler, args=(client,)).start()
 
 if __name__ == "__main__":
     main()
